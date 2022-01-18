9745f61fc400afe3aef46813bbcad67367c79021
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2022-01-18 18:47:18 +0530

    Creating contact in address book

---
+++ AddressBook/src/com/addressbook/Contact.java
@@ -0,0 +1,40 @@
+package com.addressbook;
+
+public class Contact {
+
+	public void setFirstName(String validateFirstName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEmail(String validateEmail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPhoneNumber(String validatePhone) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCity(String validateCity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setZip(String validateZip) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setState(String validateState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
